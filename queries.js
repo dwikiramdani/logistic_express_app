d9a6b24e18a86f6672ec46be604e0ffda5f55141
dwikiiramdani <official.dwikiramdani@gmail.com>
2023-08-16 21:51:25 +0700

    WIP PostgreSql auth and crud

---
+++ queries.js
@@ -0,0 +1,28 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user: 'me',
+    host:'localhost',
+    database: 'api',
+    password: '11223344',
+    port: 5432,
+})
+
+const getUsers = (request, response) => {
+    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+        if(error){
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const getUeserById = (request, response) => {
+    const id = parseInt(request.params.id)
+
+    pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+        if(error){
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
